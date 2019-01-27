6e5ccd22832d9bfffca2473824b841c7e09edddd
Spare Parts <Exitserver@users.noreply.github.com>
2019-01-27 15:16:15 -0500

    Add files via upload

---
+++ whoAreYou.js
@@ -0,0 +1,13 @@
+"use strict"; //has to go at the top
+
+alert("Hello, tell me about yourself!");
+
+var yourName = prompt("What's your name?", "");
+
+alert(`Nice to meet you, ${yourName}`);
+
+var yourSign = prompt("What's your sign?", "");
+
+alert(`${yourSign} are really cool.`);
+
+var talkLater = confirm("Let's talk some more soon!");
